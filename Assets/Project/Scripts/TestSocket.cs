15d9bedfe84083d824d25130e66099ed866dc341
peregurin <chinmayvirmani@gmail.com>
2020-10-10 19:19:53 +0530

    setup node-unity-comms

---
+++ Assets/Project/Scripts/TestSocket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class TestSocket : MonoBehaviour
+{
+    public SocketIOComponent socket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket.Connect();
+        socket.On("connection", OnConnected);
+        socket.On("error", OnError);
+    }
+
+    void OnConnected(SocketIOEvent evt)
+    {
+        Debug.Log("connected to server" + evt);
+    }
+
+    void OnError(SocketIOEvent evt)
+    {
+        Debug.Log("server connection error: " + evt);
+    }
+}
